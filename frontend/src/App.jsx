81c4dbf1490efcfc8e3404414ae91fdc4e345e38
jmokrzycki <jarek.mokrzycki@gmail.com>
2023-10-22 21:44:47 +0200

    refactor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Games from "./Games";
-// import Game from "./Game";
+import Game from "./Game";
+import Login from "./Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/games",
+    element: <Games />,
+  },
+  {
+    path: "/game/:code",
+    element: <Game />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Games />
+      <RouterProvider router={router} />
     </div>
   );
 }
